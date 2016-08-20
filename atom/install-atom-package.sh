b52b7a00c32719170a442e3a6f9362b587197d5f
Arthur Chang <arthurc.iphone@gmail.com>
2016-08-20 18:46:33 +0800

    add intall atom package

--- atom/install-atom-package.sh
+++ atom/install-atom-package.sh
@@ -1,1 +1,3 @@
-apm install atom-beautify autocomplete-python emmet-simplified linter linter-flake8 markdown-pdf tabs-to-spaces activate-power-mode term3 atom-material-syntax atom-material-ui
+sudo pacman -S python-pip
+sudo pip install pep8
+apm install atom-beautify autocomplete-python emmet-simplified linter linter-pep8 markdown-pdf tabs-to-spaces activate-power-mode term3 atom-material-syntax atom-material-ui atom-html-preview
