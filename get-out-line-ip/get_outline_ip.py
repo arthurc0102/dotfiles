7ea656007015c9d7664bb9aba81c40f0fecd78b6
Arthur Chang <arthurc.iphone@gmail.com>
2016-09-02 14:43:25 +0800

    add get outline ip

---
+++ get-out-line-ip/get_outline_ip.py
@@ -0,0 +1,47 @@
+import requests
+import os
+from html.parser import HTMLParser
+
+
+class get_ip_HTMLParser(HTMLParser):
+    in_ip_tag = False
+    ip    = ''
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'font':
+            for attr in attrs:
+                if attr[0] == 'color' and attr[1] == 'green':
+                    self.in_ip_tag = True
+
+    def handle_endtag(self, tag):
+        pass
+
+    def handle_data(self, data):
+        if self.in_ip_tag:
+            self.ip    = data
+            self.in_ip_tag = False
+
+
+def main():
+    get_ip = get_ip_HTMLParser()
+    url    = 'http://myip.com.tw/'
+
+    try:
+        html   = requests.get(url, verify=False)
+    except Exception as e:
+        print('No network')
+        return
+
+    get_ip.feed(html.text)
+
+    out_ip = get_ip.ip
+    nat_ip = os.popen('hostname -i').read().replace('\n', '')
+
+    if out_ip == nat_ip:
+        print('ip: ' + out_ip)
+    else:
+        print('out ip: ' + out_ip + '\nnat ip: ' + nat_ip)
+
+
+if __name__ == '__main__':
+    main()
