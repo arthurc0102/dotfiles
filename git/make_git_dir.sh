ab5fe00b01804652bb67990bd8268f6ee02850ea
Arthur Chang <arthurc.iphone@gmail.com>
2016-09-22 09:29:32 +0800

    let readme have title

--- git/make_git_dir.sh
+++ git/make_git_dir.sh
@@ -2,7 +2,7 @@ if [ $# == 1 ]; then
   mkdir ${1}
   cd ${1}
   git init >> /dev/null
-  touch README.md
+  echo "# "${1} >> README.md
   git add . >> /dev/null
   git commit -m "init" >> /dev/null
 else
