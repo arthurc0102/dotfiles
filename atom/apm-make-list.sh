cb03276ee62144fc740f739395d94e937e40ff49
Arthur Chang <arthurc.iphone@gmail.com>
2017-01-31 14:15:32 +0800

    atom backup

---
+++ atom/apm-make-list.sh
@@ -0,0 +1,1 @@
+apm list --installed --bare > packages.list
--- atom/install-atom-package.sh
+++
@@ -1,3 +0,0 @@
-sudo pacman -S python-pip
-sudo pip install pep8
-apm install atom-beautify autocomplete-python emmet-simplified linter linter-pep8 markdown-pdf tabs-to-spaces activate-power-mode atom-material-syntax atom-material-ui atom-html-preview atom-sublime-monokai-syntax term3 indent-guide-improved python-indent
