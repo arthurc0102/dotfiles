357e3d2a985966a9ad4a03f664c5d68318581cf6
Arthur Chang <arthurc.iphone@gmail.com>
2017-03-13 23:42:41 +0800

    update check-update.sh

--- script/check-update.sh
+++ script/check-update.sh
@@ -1,5 +1,11 @@
+echo -e "========== Checking pacman update ...... ================"
 sudo pacman -Syu
+echo
 
+echo -e "========== Checking AUR update ...... ==================="
 yaourt -Syu --aur
+echo
 
+echo -e "========== Checking atom package update ...... =========="
 apm update
+echo
