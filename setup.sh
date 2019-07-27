7fde5ff700cf5183376da50291e8651c6bec1e95
Arthur Chang <arthurc0102@gmail.com>
2019-07-28 02:00:04 +0800

    Update url

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 command -v git > /dev/null || { echo "Git not installed"; exit 1; }
 
 if [ ! -d "$HOME/.dotfiles" ]; then
-    git clone https://github.com/arthurc0102/dotfiles.git $HOME/.dotfiles
+    git clone git@github.com:arthurc0102/dotfiles.git $HOME/.dotfiles
 fi
 
 cd $HOME/.dotfiles
