c08904ecd3a9b9b625524de5b5fe8bbb26ba2c2e
Arthur Chang <arthurc0102@gmail.com>
2020-03-15 15:01:07 +0800

    Remove lazyload to fix pipenv error

--- zshrc.zsh
+++ zshrc.zsh
@@ -9,7 +9,6 @@ antigen bundle pip
 antigen bundle docker
 antigen bundle docker-compose
 antigen bundle web-search
-antigen bundle qoomon/zsh-lazyload
 antigen bundle zsh-users/zsh-completions
 antigen bundle zsh-users/zsh-autosuggestions
 antigen bundle zsh-users/zsh-syntax-highlighting
@@ -28,5 +27,4 @@ source "$HOME/.dotfiles/zsh/environmentals/android.zsh"
 source "$HOME/.dotfiles/zsh/environmentals/flutter.zsh"
 source "$HOME/.dotfiles/zsh/environmentals/mysql.zsh"
 source "$HOME/.dotfiles/zsh/environmentals/golang.zsh"
-
-lazyload pyenv python pip pipenv poetry -- 'source "$HOME/.dotfiles/zsh/environmentals/python.zsh"'
+source "$HOME/.dotfiles/zsh/environmentals/python.zsh"
