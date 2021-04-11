b53e815eaf82347f101addb6784b17e34b03c2cb
Arthur Chang <arthurc0102@gmail.com>
2021-04-12 00:44:10 +0800

    Add gcloud

---
+++ zsh/environmentals/gcloud.zsh
@@ -0,0 +1,5 @@
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '~/.gcloud/path.zsh.inc' ]; then . '~/.gcloud/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '~/.gcloud/completion.zsh.inc' ]; then . '~/.gcloud/completion.zsh.inc'; fi
