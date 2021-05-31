98582d4b1565c9075930e34a5ab8b6e8bb00d541
Arthur Chang <arthurc0102@gmail.com>
2021-05-31 19:15:14 +0800

    Remove pyenv virtualenv plugin

--- zsh/environmentals/python.zsh
+++ zsh/environmentals/python.zsh
@@ -16,7 +16,7 @@ load_pyenv() {
   if command -v pyenv 1>/dev/null 2>&1; then
     eval "$(pyenv init --path)"
     eval "$(pyenv init -)"
-    eval "$(pyenv virtualenv-init -)"
+    # eval "$(pyenv virtualenv-init -)"
   fi
 }
 
