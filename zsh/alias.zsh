8bcdf3e1b8d11fbc444dd1644906c2e46aa3d07a
Arthur Chang <arthurc0102@gmail.com>
2021-06-02 12:52:58 +0800

    Update alias tc

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -20,8 +20,8 @@ alias gs='git status'
 alias gz='git-cz'  # full command to target lazyload
 alias gitignore="git-ignore"
 
-# Remove last newline char and add to clipboard
-alias tc='python -c "import sys; print(\"\\n\".join(sys.stdin.read().splitlines()), end=\"\")" | pbcopy'
+# Remove last newline char and add to clipboard. ('\r\n' work on Mac, Linux and Windows)
+alias tc='python -c "import sys; print(sys.stdin.read().rstrip(\"\r\n\"), end=\"\")" | pbcopy'
 
 # Python
 alias pip-activate="source ./.venv/bin/activate"
