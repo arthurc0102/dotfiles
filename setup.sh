708671c202aeba42ecc9b914682db75175a10483
Arthur Chang <arthurc0102@gmail.com>
2021-08-04 14:59:46 +0800

    Add echo for function

--- setup.sh
+++ setup.sh
@@ -2,22 +2,26 @@
 set -e
 
 check() {
+    echo "Check."
     command -v git > /dev/null || { echo "Git not installed"; exit 1; }
 }
 
 setup_ohmyzsh() {
+    echo "Setup oh-my-zsh"
     if [ ! -d "$HOME/.oh-my-zsh" ]; then
         git clone https://github.com/ohmyzsh/ohmyzsh.git $HOME/.oh-my-zsh
     fi
 }
 
 setup_dotfiles() {
+    echo "Setup dotfiles"
     if [ ! -d "$HOME/.dotfiles" ]; then
         git clone https://github.com/arthurc0102/dotfiles.git $HOME/.dotfiles
     fi
 }
 
 setup_tmux() {
+    echo "Setup tmux"
     if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
         git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
     fi
@@ -27,6 +31,7 @@ setup_tmux() {
 }
 
 setup_git() {
+    echo "Setup git"
     ln -sf $HOME/.dotfiles/git/gitconfig $HOME/.gitconfig
     cp $HOME/.dotfiles/git/gitconfig.user $HOME/.gitconfig.user
 }
@@ -36,6 +41,8 @@ setup_karabiner() {
         return;
     fi
 
+    echo "Setup karabiner"
+
     if [ ! -d "$HOME/.config/karabiner" ]; then
         mkdir -p $HOME/.config/karabiner
     fi
@@ -44,6 +51,7 @@ setup_karabiner() {
 }
 
 setup_zsh() {
+    echo "Setup zsh"
     ln -sf $HOME/.dotfiles/zsh/zshrc.zsh $HOME/.zshrc
     ln -sf $HOME/.dotfiles/zsh/zshenv.zsh $HOME/.zshenv
     cp $HOME/.dotfiles/zsh/zshenv.local.zsh $HOME/.zshenv.local
