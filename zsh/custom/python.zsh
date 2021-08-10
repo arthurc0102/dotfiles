a8991d52afeccd8571f315dd86ab1176629121f1
Arthur Chang <arthurc0102@gmail.com>
2021-08-10 17:47:11 +0800

    Disable automatic pipenv shell activation/deactivation

--- zsh/custom/python.zsh
+++ zsh/custom/python.zsh
@@ -1,5 +1,7 @@
 # Pipenv
 export PIPENV_VENV_IN_PROJECT=true
 
+add-zsh-hook -d chpwd _togglePipenvShell  # Disable automatic pipenv shell activation/deactivation
+
 # Pipx
 eval "$(register-python-argcomplete pipx)"
