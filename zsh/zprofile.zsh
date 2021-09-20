d4b0bd24d0b0279ca285da19de943e71ea036ac1
Arthur Chang <arthurc0102@gmail.com>
2021-09-20 20:54:17 +0800

    Fix zprofile error

--- zsh/zprofile.zsh
+++ zsh/zprofile.zsh
@@ -15,7 +15,7 @@ fi
 
 # Pyenv Config
 export PYENV_ROOT="$HOME/.pyenv"
-if [ -d $PYENV_ROOT ] then
+if [ -d $PYENV_ROOT ]; then
     export PATH="$PYENV_ROOT/bin:$PATH"
     eval "$(pyenv init --path)"
 fi
