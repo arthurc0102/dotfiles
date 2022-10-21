bcbbc5164f0a540cb5e8262fe8613f84f6ad3260
Arthur Chang <arthurc0102@gmail.com>
2022-10-22 01:58:44 +0800

    Remove not needed config

--- zsh/custom/theme.zsh
+++ zsh/custom/theme.zsh
@@ -1,10 +1,14 @@
-spaceship_rprompt_prefix() {
-  echo -n '%{'$'\e[1A''%}'
-}
-
-spaceship_rprompt_suffix() {
-  echo -n '%{'$'\e[1B''%}'
-}
+# 這兩個 function本來用來處理右邊的資訊出現在第二行的問題
+# 不過後台 theme 修好了這個問題所以就用不到了
+# 如果還需要用到在 SPACESHIP_RPROMPT_ORDER 開頭加上 rprompt_prefix 結尾加上 rprompt_suffix
+#
+# spaceship_rprompt_prefix() {
+#   echo -n '%{'$'\e[1A''%}'
+# }
+#
+# spaceship_rprompt_suffix() {
+#   echo -n '%{'$'\e[1B''%}'
+# }
 
 SPACESHIP_CHAR_SYMBOL='$ '
 SPACESHIP_VENV_PREFIX='('
@@ -16,5 +20,5 @@ SPACESHIP_GIT_SYMBOL=''
 SPACESHIP_EXIT_CODE_SHOW='true'
 SPACESHIP_TIME_SHOW='true'
 SPACESHIP_EXEC_TIME_ELAPSED='0'
-SPACESHIP_RPROMPT_ORDER=(rprompt_prefix exit_code time exec_time rprompt_suffix)
+SPACESHIP_RPROMPT_ORDER=(exit_code time exec_time)
 SPACESHIP_PROMPT_ORDER=(user host dir git line_sep venv char)
