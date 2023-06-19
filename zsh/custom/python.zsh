9b1a871a50485e55310b4c7b35d65e4715247378
Arthur Chang <arthurc0102@gmail.com>
2023-06-19 16:46:22 +0800

    Update register-python-argcomplete not found message

--- zsh/custom/python.zsh
+++ zsh/custom/python.zsh
@@ -7,5 +7,5 @@ add-zsh-hook -d chpwd _togglePipenvShell  # Disable automatic pipenv shell activ
 if command -v register-python-argcomplete > /dev/null; then
     eval "$(register-python-argcomplete pipx)"
 else
-    echo "Command 'register-python-argcomplete' not found. If you need autocomplete for 'pipx' install it."
+    echo "Command 'register-python-argcomplete' not found. If you need autocomplete for 'pipx' install package 'argcomplete'."
 fi
