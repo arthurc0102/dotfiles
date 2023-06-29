cef814a4cbba83a0e1c9380a081f36e814eb2551
Arthur Chang <arthurc0102@gmail.com>
2023-06-29 13:41:00 +0800

    Remove git-cz config

--- setup.sh
+++ setup.sh
@@ -33,7 +33,6 @@ setup_tmux() {
 setup_git() {
     echo "Setup git"
     ln -svf $HOME/.dotfiles/git/gitconfig $HOME/.gitconfig
-    ln -svf $HOME/.dotfiles/git/git-cz.json $HOME/.git-cz.json
     cp -v $HOME/.dotfiles/git/gitconfig.user $HOME/.gitconfig.user
 }
 
--- zsh/custom/alias.zsh
+++ zsh/custom/alias.zsh
@@ -3,7 +3,6 @@ alias fd='echo ${PWD##*/}'
 alias pp='ping 8.8.8.8'
 alias pd='ping fb.me'
 
-alias gz='git-cz'  # full command to target lazyload
 alias gitignore="git-ignore"
 
 # Remove last newline char and add to clipboard. ('\r\n' work on Mac, Linux and Windows)
