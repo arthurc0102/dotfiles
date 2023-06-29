7d80b789eb103fb3b27f5ec2864c9baf23f382eb
Arthur Chang <arthurc0102@gmail.com>
2023-06-29 13:42:04 +0800

    Setup autocomplete only if pipx command exists

--- zsh/custom/python.zsh
+++ zsh/custom/python.zsh
@@ -1,6 +1,8 @@
 # Pipx
-if command -v register-python-argcomplete > /dev/null; then
-    eval "$(register-python-argcomplete pipx)"
-else
-    echo "Command 'register-python-argcomplete' not found. If you need autocomplete for 'pipx' install package 'argcomplete'."
+if command -v pipx > /dev/null; then
+    if command -v register-python-argcomplete > /dev/null; then
+        eval "$(register-python-argcomplete pipx)"
+    else
+        echo "Command 'register-python-argcomplete' not found. If you need autocomplete for 'pipx' install package 'argcomplete'."
+    fi
 fi
