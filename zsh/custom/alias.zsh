4427f04ccde0ac7764cee12c91212c7384c0b3a1
Arthur Chang <arthurc0102@gmail.com>
2023-07-30 18:43:13 +0800

    Add emacs alias

--- zsh/custom/alias.zsh
+++ zsh/custom/alias.zsh
@@ -5,3 +5,7 @@ alias pd='ping fb.me'
 
 alias gz='git-cz'  # Install with 'npm install -g commitizen'
 alias gitignore="git-ignore"
+
+alias emacs-origin="$(which emacs)"  # Backup origin emacs command
+alias emacs-kill-server='emacsclient -e "(kill-emacs)"'
+alias emacs='emacsclient --alternate-editor="" --create-frame'
