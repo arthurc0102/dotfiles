77814cccca7cf74d7f367f28c2c40dfb7e6249e0
Arthur Chang <arthurc0102@gmail.com>
2023-08-08 10:43:49 +0800

    Remove emacsclient alias

--- zsh/custom/alias.zsh
+++ zsh/custom/alias.zsh
@@ -5,7 +5,3 @@ alias pd='ping fb.me'
 
 alias gz='git-cz'  # Install with 'npm install -g commitizen'
 alias gitignore="git-ignore"
-
-alias emacs-origin="$(which emacs)"  # Backup origin emacs command
-alias emacs-kill-server='emacsclient -e "(kill-emacs)"'
-alias emacs='emacsclient --alternate-editor="" --create-frame'
