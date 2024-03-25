9f5dcba5fe179027332f58ca67973058f7f66bb1
Arthur Chang <arthurc0102@gmail.com>
2024-03-25 14:55:02 +0800

    Setup git lfs and alias

--- zsh/custom/alias.zsh
+++ zsh/custom/alias.zsh
@@ -3,4 +3,5 @@ alias pd='ping fb.me'
 
 alias gz='git-cz'  # Install with 'npm install -g commitizen'
 alias gitignore="git-ignore"
-alias gdss='git -c delta.side-by-side=true diff'
+alias gdf='git diff'
+alias gd='git -c delta.side-by-side=true diff'
