1df04752d14e171259bc290802d68c32bc7491dd
Arthur Chang <arthurc0102@gmail.com>
2024-04-16 13:12:46 +0800

    Update alias

--- zsh/custom/alias.zsh
+++ zsh/custom/alias.zsh
@@ -1,8 +1,15 @@
+# Ping
 alias pp='ping 8.8.8.8'
 alias pd='ping fb.me'
 
+# Git
 alias gz='git-cz'  # Install with 'npm install -g commitizen'
 alias gitignore="git-ignore"
 alias gdf='git diff'
 alias gd='git -c delta.side-by-side=true diff'
+
+# Folder
 alias current_dir='echo ${PWD##*/}'
+
+# Python
+alias python-executable-path='python -c "import sys; print(sys.executable)"'
