b6c3d9e41e59430811c93c5ac833c72ab34497f4
Arthur Chang <arthurc0102@gmail.com>
2024-09-06 17:12:04 +0800

    Update alias

--- zsh/custom/alias.zsh
+++ zsh/custom/alias.zsh
@@ -3,8 +3,13 @@ alias pp='ping 8.8.8.8'
 alias pd='ping fb.me'
 
 # Git
-alias gz='git-cz'  # Install with 'npm install -g commitizen'
-alias gitignore="git-ignore"
+if command -v git-cz > /dev/null; then
+    alias gz='git-cz'  # Install with 'npm install -g commitizen'
+elif command -v uvx > /dev/null; then
+    alias gz='uvx --from=commitizen cz c'
+fi
+
+alias gitignore='git-ignore'
 alias gdf='git diff'
 alias gd='git -c delta.side-by-side=true diff'
 
